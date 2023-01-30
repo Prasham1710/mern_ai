2a0061da6f651e07779d68eb834d5607643a461f
Prasham1710 <jainprasham17@gmail.com>
2023-01-30 22:53:46 +0530

    mongo connected

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import * as dotev from 'dotenv';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import connectDB from './mongodb/connect';
+import connectDB from './mongodb/connect.js';
+
 
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
@@ -13,13 +14,15 @@ app.get('/', async (req, res) => {
     res.send('Hello');
 })
 
-const startServer = async () => {
+
+
+async function startServer() {
 
     try {
         connectDB(process.env.MONGODB_URL);
-        app.listen(8080, () => console.log('Server started on port http://localhost:8080'))
-    }catch(error){
-        console.log(error)
-        }
+        app.listen(8080, () => console.log('Server started on port http://localhost:8080'));
+    } catch (error) {
+        console.log(error);
+    }
 }
   startServer();

480abb2f100a899788096c29b674a3d62ab5994c
Prasham1710 <jainprasham17@gmail.com>
2023-01-30 23:24:50 +0530

    backend completed

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 
 dotenv.config();
@@ -10,6 +12,8 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 app.get('/', async (req, res) => {
     res.send('Hello');
 })
